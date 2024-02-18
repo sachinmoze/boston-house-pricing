bbd2d2ddd691e67efa235fcaa776579d1fe430d6
sachinmoze <mozesachin02@gmail.com>
2024-02-18 22:47:31 +0530

    commiting new change

--- myproject/settings.py
+++ myproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-o+*=q^zs4$s6ht0c6#rso-&je))@bj74svi8ri(t25z91!bpzn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
